f9147ed2e7b90cddcf7e54e5d45612c423d29cfd
filip_wierzbicki <filip_wierzbicki@epam.com>
2022-09-06 19:38:08 +0200

    Level_2 and Level_3

--- src/Level_2.java
+++ src/Level_2.java
@@ -1,14 +1,5 @@
 public class Level_2 {
 
-//    Modularization
-//    Reuse the created functions and create test cases which call them with various input to achieve the best coverage
-//    using black-box and white-box techniques. Extract the functions into their own module.
-//    Techniques worth considering: BVA, EP, MC/DC
-
-//            Calculator
-//    Create a program which takes two numbers as input and then calculates the results of the 4 basic operands(+, -, *, /).
-//    Create separate functions for the 4 operands and use them in a modular fashion.
-
     public static double calculatorAdd(double x, double y){
         return x+y;
     }
@@ -24,6 +15,5 @@ public class Level_2 {
     public static double calculatorDivide(double x, double y){
         return x/y;
     }
-
 
 }
---
+++ src/Level_3.java
@@ -0,0 +1,66 @@
+import java.util.Locale;
+
+public class Level_3 {
+
+    public static boolean palindrome(String word) {
+
+        word = word.toLowerCase(Locale.ROOT);
+        word = word.replaceAll("[-+.^; :,']", "");
+
+        char[] wordArray = new char[word.length()];
+        for (int i = 0; i < wordArray.length; i++) {
+            wordArray[i] = word.charAt(i);
+        }
+
+        for (int i = 0; i < word.length() / 2; i++) {
+            char temp = wordArray[i];
+            wordArray[i] = wordArray[wordArray.length - i - 1];
+            wordArray[word.length() - i - 1] = temp;
+        }
+
+        String result = new String(wordArray);
+
+        if (word.equals(result)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static int[] prisonGuardProblem(int n, int m) {
+
+        int[] prison = new int[m];
+
+        for (int i = 0; i < m; i++) {
+            prison[i] = 0;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            if (prison[i+1] % i == 0){
+                if (prison[i+1] == 0){
+                    prison[i+1] = 1;
+                } else if (prison[i+1] == 1){
+                    prison[i+1] = 0;
+                }
+
+            }
+        }
+
+        return prison;
+    }
+}
+
+//    Prison guard problem
+//        You have N prison guards and M cell gates. The status of the gates can be open (1) or closed (0).
+//        At the beginning, all the cell gates are closed.
+//        Every prison guard go through the cell gates opens the closed gates and closes the opened gates
+//        the following way:
+//        first prison guard goes through all the gates and opens all of them
+//        second prison guard goes through every second cell gate and closes the open ones
+//        third prison guard goes through every third cell gate and opens the closed ones, and closes the opened ones etc.
+//        Your task is to write an application which gets the N (guard number) and M (gate number) as a parameter and
+//        returns an int[] with the status of the cell gates after all the guards went through the gates.
+//        Sample input:
+//        N = 5, M = 5
+//        Sample output:
+//        1, 0, 0, 1, 0
